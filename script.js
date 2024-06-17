89f5f71d4569b3e944a239f98d359d5773c49e13
Dainerdev <florianarroyo15@gmail.com>
2024-06-17 16:12:29 -0500

    Web con Bootstrap

---
+++ script.js
@@ -0,0 +1,7 @@
+// Obtener el botón
+var btnArriba = document.getElementById("btnArriba");
+
+// Cuando el usuario hace clic en el botón, desplazarse hacia arriba de la página
+function topFunction() {
+    document.documentElement.scrollTop = 0; 
+}
